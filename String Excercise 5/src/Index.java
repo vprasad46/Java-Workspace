fdf04b521674197aac3e91a1bedf3b31d019580f
vprasad46 <vprasad46@gmail.com>
2017-09-21 14:39:15 +0530

    String Excercise 5

---
+++ String Excercise 5/src/Index.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Index {
+
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		String string = new String();
+		
+		System.out.println("::Enter the encoded pattern::");
+		string = input.nextLine();
+		int a[] = new int[26];
+		int i=0;
+		while(i<string.length()){
+			if(i+2 < string.length()){
+				if(string.charAt(i+2) == '#'){
+					a[Integer.parseInt(string.substring(i,i+2))-1]++;
+					i += 3;
+				}
+				else if(string.charAt(i+2) != '#'){
+					a[Character.getNumericValue(string.charAt(i))-1]++;
+					i+=1;
+				}
+			}
+			else{
+				a[Character.getNumericValue(string.charAt(i))-1]++;
+				a[Character.getNumericValue(string.charAt(i+1))-1]++;
+				i+=2;
+			}
+			
+		}
+		for(int j =0;j<a.length;j++)
+			System.out.print(a[j]+" ");
+		
+	}
+}
